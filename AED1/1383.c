f971fe029aaa64ab5af40674a9c599112f6c7676
unknown <josevitorrequena@gmail.com>
2024-10-28 22:07:27 -0300

    sudoku

---
+++ AED1/1383.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int VerificaMatriz(int **mat, int start, int n){
+    for(int j = 0; j < start; j++){
+        int *tempRow = (int*)calloc(9, sizeof(int));
+        int *tempCol = (int*)calloc(9, sizeof(int));
+        for(int k = 0; k < start; k++){
+            tempRow[mat[j][k]-1]++;
+            tempCol[mat[k][j]-1]++;
+        }
+        for(int l = 0; l < 9; l++){
+            if(tempCol[l]!=1 || tempRow[l]!=1){
+                printf("Instancia %d \n", n);
+                printf("NAO\n\n");
+                return 0;
+            } else {
+                printf("Instancia %d \n", n);
+                printf("SIM\n\n");
+                return 0;
+            }
+        }
+        free(tempCol);
+        free(tempRow);
+    }
+    return 0;
+}
+
+int main(){
+    int n;
+    // Recebe número de jogos n e alloca uma matriz do tamanho dos jogos
+    scanf("%d", &n);
+    int **mat = (int **)malloc((9 * n) * sizeof(int*));
+    for (int i = 0; i < 9*n; ++i) mat[i] = (int *)malloc(9 * sizeof(int));
+    // Recebe a matriz por input
+    for (int i = 0; i < 9*n; i++){
+        for (int j = 0; j < 9; j++){
+            scanf("%d", &mat[i][j]);
+        }  
+    }
+    // Verifica a Matriz
+    for(int i = 0; i < n; i++){
+        VerificaMatriz(mat,9*n,n);
+    }
+    free(mat);
+    return 0;
+}
